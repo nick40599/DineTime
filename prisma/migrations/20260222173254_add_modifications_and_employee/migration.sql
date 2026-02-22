f4abbda318ba4815a41dae4c0d196df53ddec57f
Nick Sullivan <nicksull42@gmail.com>
2026-02-22 11:35:14 -0600

    Prisma migrations for modifications and employees

---
+++ prisma/migrations/20260222173254_add_modifications_and_employee/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Modifications" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "position" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "pin" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
